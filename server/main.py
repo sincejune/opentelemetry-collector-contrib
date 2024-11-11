dd133f2c0a4610c9e75c9a45eea96e871b5ee3ec
Chao <chao.weng@appdynamics.com>
2024-11-11 22:00:50 +0800

    Fix

--- server/main.py
+++ server/main.py
@@ -23,7 +23,7 @@ async def query():
             "group_by_query_hash": {
                 "terms": {
                     "field": "query_hash",
-                    "size": 10
+                    "size": 50
                 }
             }
         }
@@ -31,17 +31,27 @@ async def query():
     response = es.search(index="metrics-sqlserverreceiver-default", body=query_body)
 
     # Print the aggregation results
-    print("Aggregation Results:")
+    data = {}
     for bucket in response["aggregations"]["group_by_query_hash"]["buckets"]:
-        print(bucket)
-        print(f"query_hash: {bucket['key']}, Doc Count: {bucket['doc_count']}")
+        query_hash = bucket['key']
+        if query_hash == "" or query_hash is None:
+            continue
+        q2 = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"term": {"isplanquery": "no"}},
+                        {"term": {"query_hash": query_hash}}
+                    ]
+                }
+            },
+            "size": 1
+        }
+        response2 = es.search(index="metrics-sqlserverreceiver-default", body=q2)
+        statement = response2["hits"]["hits"][0]["_source"]["statement"]
+        data[query_hash] = statement
 
-    # Print the document hits
-    print("Document Hits:")
-    for hit in response["hits"]["hits"]:
-        print(hit["_source"])
-    return [x['key'] for x in response["aggregations"]["group_by_query_hash"]["buckets"]]
-
+    return data
 
 @app.get("/query/{query_hash}", response_class=PlainTextResponse)
 async def query(query_hash: str):
@@ -87,3 +97,9 @@ async def query(query_hash: str):
             return {"message": "No documents found"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1")
