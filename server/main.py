20e9ab8b129aa63b7264e52ced6d448e517632f6
Chao <chao.weng@appdynamics.com>
2024-11-11 22:19:26 +0800

    Fix

--- server/main.py
+++ server/main.py
@@ -76,6 +76,65 @@ async def query(query_hash: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+
+@app.get("/queryplan")
+async def queryplan():
+    query_body = {
+        "query": {
+            "term": {
+                "isplanquery": "no"
+            }
+        },
+        "aggs": {
+            "group_by_query_hash": {
+                "terms": {
+                    "field": "query_hash",
+                    "size": 50
+                }
+            }
+        }
+    }
+    response = es.search(index="metrics-sqlserverreceiver-default", body=query_body)
+
+    # Print the aggregation results
+    data = {}
+    for bucket in response["aggregations"]["group_by_query_hash"]["buckets"]:
+        query_hash = bucket['key']
+        if query_hash == "" or query_hash is None:
+            continue
+        q2 = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"term": {"isplanquery": "no"}},
+                        {"term": {"query_hash": query_hash}}
+                    ]
+                }
+            },
+            "size": 1
+        }
+        response2 = es.search(index="metrics-sqlserverreceiver-default", body=q2)
+        statement = response2["hits"]["hits"][0]["_source"]["statement"]
+
+        q3 = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"term": {"isplanquery": "yes"}},
+                        {"term": {"query_hash": query_hash}}
+                    ]
+                }
+            },
+            "size": 1
+        }
+
+        response3 = es.search(index="metrics-sqlserverreceiver-default", body=q3)
+        query_plan = response3["hits"]["hits"][0]["_source"]["query_plan"]
+        data[query_hash] = {"statement": statement, "query_plan": query_plan}
+
+    return data
+
+
 @app.get("/queryplan/{query_hash}", response_class=PlainTextResponse)
 async def query(query_hash: str):
     query_body = {
